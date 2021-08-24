fb99f4a988477a979a31cedcb536ecb462ef1e85
Frank Owusu Boakye <oboy.nitro1@gmail.com>
2021-08-24 13:59:03 -0700

    updated directory name

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'b029404cd3b740e52919bbe9957a95e26629ac56',
         'name' => 'oboynitro/flexy-orm',
         'dev' => true,
     ),
     'versions' => array(
         'oboynitro/flexy-orm' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'b029404cd3b740e52919bbe9957a95e26629ac56',
             'dev_requirement' => false,
         ),
     ),
